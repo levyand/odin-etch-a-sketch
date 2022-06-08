3130d760aed170fee75a5b139d43e59567bd0c90
Andrew Levy <levyand@gmail.com>
2022-06-07 22:47:54 -0500

    add drawing functionality to blocks on hover

--- script.js
+++ script.js
@@ -9,6 +9,14 @@ function setupGrid(dimension) {
   for (let i = 0; i < blockCount; i++) {
     const block  = document.createElement('div');
     block.classList.add('block');
+    block.addEventListener('mouseover', (e) => {
+      const block = e.target;
+
+      if (block.classList.contains('active')) return;
+
+      block.classList.add('active');
+    });
+
     container.appendChild(block);
   }
 }
